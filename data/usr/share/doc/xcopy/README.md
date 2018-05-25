6c8d8c8991c90976a474e10b0bdfb3378c1775f4
zvezdochiot <mykaralw@yandex.ru>
2018-05-25 18:43:36 +0300

    0.20180525
    Options Version

---
+++ data/usr/share/doc/xcopy/README.md
@@ -0,0 +1,19 @@
+# XCopy
+
+Bash-script for copy dir as sort structure.
+
+https://github.com/zvezdochiot/bash-xcopy
+
+## Make deb package
+
+```
+du -s
+nano config/control #edit config
+sudo bash-deb-build lzma
+```
+
+## Utilites
+
+For build deb package install bash-deb-build:
+
+https://github.com/zvezdochiot/bash-deb-build
--- data/usr/share/scripts/bash/xcopy.sh
+++ data/usr/share/scripts/bash/xcopy.sh
@@ -1,17 +1,72 @@
 #!/bin/sh
 
 #xcopy.sh
-#2015-11-18
+#2018-05-25
 
+echo "XCopy 0.20180525"
+
+tnocomp=""
+tcomp="/bin/cpio"
+tdeb="cpio_*.deb"
+if [ ! -f "$tcomp" ]
+then
+    tnocomp="$tnocomp $tcomp($tdeb)"
+fi
+tcomp="/usr/bin/find"
+tdeb="findutils_*.deb"
+if [ ! -f "$tcomp" ]
+then
+    tnocomp="$tnocomp $tcomp($tdeb)"
+fi
+tcomp="/usr/bin/sort"
+tdeb="coreutils_*.deb"
+if [ ! -f "$tcomp" ]
+then
+    tnocomp="$tnocomp $tcomp($tdeb)"
+fi
+tcomp="/bin/ln"
+tdeb="coreutils_*.deb"
+if [ ! -f "$tcomp" ]
+then
+    tnocomp="$tnocomp $tcomp($tdeb)"
+fi
+if [ "x$tnocomp" != "x" ]
+then
+    echo "Not found $tnocomp !"
+    echo ""
+    exit 1
+fi
+
+fln="false"
+fnc="false"
+fhlp="false"
+while getopts ":lnh" opt
+do
+    case $opt in
+        l) fln="true"
+            ;;
+        n) fnc="true"
+            ;;
+        h) fhlp="true"
+            ;;
+        *) echo "Unknown option -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+shift "$(($OPTIND - 1))"
 tdir=`pwd`;
 srcdir="$1";
 dstdir="$2";
-flgcp="$3";
 
-if [ "x$srcdir" = "x" -o "x$dstdir" = "x" ]
+if [ "x$srcdir" = "x" -o "x$dstdir" = "x" -o "x$fhlp" = "xtrue" ]
 then
     echo "Usage:"
-    echo "$0 srcdir destdir [nc|ln]"
+    echo "$0 [options] srcdir destdir"
+    echo "Options:"
+    echo "    -l    hardlink files"
+    echo "    -n    no copy files (only directory)"
+    echo "    -h    help"    
     exit 0
 fi
 
@@ -29,14 +84,16 @@ fi
 # tree dirs
 find "$srcdir" -xtype d | sort | cpio -padv "$dstdir" ;
 
-# copy file
-if [ "x$flgcp" = "x" ]
+# copy/link file
+if [ "x$fnc" = "xfalse" ]
 then
-    find "$srcdir" -xtype f | sort | cpio -padv "$dstdir";
-elif [ "x$flgcp" = "xln" ]
-then
-    find "$srcdir" -xtype f -printf "%p\n" | sort | while read tfile
-    do
-        ln -fv "$tfile" "$dstdir/$tfile"
-    done
+    if [ "x$fln" = "xfalse" ]
+    then
+        find "$srcdir" -xtype f | sort | cpio -padv "$dstdir";
+    else
+        find "$srcdir" -xtype f -printf "%p\n" | sort | while read tfile
+        do
+            ln -fv "$tfile" "$dstdir/$tfile"
+        done
+    fi
 fi
